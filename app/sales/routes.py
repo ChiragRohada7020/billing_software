de553dedc317d88ab3b55eb34bd0ece8146b3367
Chirag <chiragrohada40@gmail.com>
2024-05-01 03:06:46 +0530

    search of sale improved

--- app/sales/routes.py
+++ app/sales/routes.py
@@ -3,10 +3,63 @@ from flask import Blueprint, jsonify, request,render_template
 from app import db
 from bson.json_util import dumps
 from datetime import datetime, timezone
+from bson import ObjectId
 
 
 
 sales_bp = Blueprint('sales', __name__)
+
+
+# app/sales/routes.py
+# app/sales/routes.py
+
+from bson.json_util import dumps
+
+
+def is_valid_objectid(search_term):
+    try:
+        ObjectId(search_term)
+        return True
+    except:
+        return False
+
+@sales_bp.route('/search', methods=['get'])
+def search_sales():
+    try:
+        search_term = request.args.get('searchTerm', '').lower()
+        print(search_term)
+        
+        query = {
+            '$or': [
+                {'customer_name': {'$regex': search_term, '$options': 'i'}},
+                {'totalAmount': {'$regex': search_term, '$options': 'i'}}
+                # Add more fields to search here if needed
+            ]
+        }
+
+        if is_valid_objectid(search_term):
+            query['$or'].append({'_id': ObjectId(search_term)})
+
+        # Perform the search
+        sales = db.sales.find(query).limit(4) # Limit to top 4 results
+        
+        suggestions = [{
+            'customer_name': sale['customer_name'],
+            'totalAmount': sale['totalAmount'],
+            'customer_id': str(sale['_id']),           
+            'payingAmount':sale['payingAmount'],
+            'payment_status':sale['payment_status'],
+            'dueAmount':sale['dueAmount'],
+            'datetime':str(sale['datetime'])
+
+
+            # Add more fields to return here if needed
+        } for sale in sales]
+        
+        return dumps(suggestions)
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
 
 
 @sales_bp.route('/', methods=['GET'])
